2e928b25ace2575a9eb63e20682dc3bb5563c92a
Gord Thompson <gord@gordthompson.com>
2019-09-03 15:44:48 -0600

    cleanups for packaging and deployment

--- setup.py
+++ setup.py
@@ -10,32 +10,31 @@ v.close()
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 
 
-setup(name='sqlalchemy_access',
+setup(name='sqlalchemy-access',
       version=VERSION,
       description="MS Access for SQLAlchemy",
       long_description=open(readme).read(),
       classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Environment :: Console',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python',
-      'Programming Language :: Python :: 3',
-      'Programming Language :: Python :: Implementation :: CPython',
-      'Topic :: Database :: Front-Ends',
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Database :: Front-Ends',
       ],
       keywords='SQLAlchemy Microsoft Access',
-      author='Mike Bayer',
-      author_email='mike@zzzcomputing.com',
+      author='Gord Thompson',
+      author_email='gord@gordthompson.com',
       license='MIT',
       packages=['sqlalchemy_access'],
       include_package_data=True,
-      # tests_require=['nose >= 0.11'],
-      # test_suite="nose.collector",
       zip_safe=False,
       entry_points={
-         'sqlalchemy.dialects': [
+          'sqlalchemy.dialects': [
               'access = sqlalchemy_access.pyodbc:AccessDialect_pyodbc',
               'access.pyodbc = sqlalchemy_access.pyodbc:AccessDialect_pyodbc',
               ]
-        }
+      },
+      install_requires = ['SQLAlchemy', 'pyodbc>=4.0.27'],
 )
