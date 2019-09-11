a52e38dcf1fd781a210bcf5b8f535a12d214347c
Gord Thompson <gord@gordthompson.com>
2019-09-11 15:08:00 -0600

    disable pyodbc.pooling for ODBC linked tables

--- sqlalchemy_access/__init__.py
+++ sqlalchemy_access/__init__.py
@@ -1,5 +1,7 @@
-__version__ = '1.0.0'
-
+import pyodbc
 from sqlalchemy.dialects import registry
 
+__version__ = '1.0.1'
+
+pyodbc.pooling = False
 registry.register("access.pyodbc", "sqlalchemy_access.pyodbc", "AccessDialect_pyodbc")
